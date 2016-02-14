5f478844bacea86b24c83a91e22e75f015313d8b
William Long <wlong0827@gmail.com>
2016-02-14 16:50:46 -0500

    Command Line arguments

--- mst.c
+++ mst.c
@@ -2,8 +2,13 @@
  
 int main(int argc, char *argv[])
 {
- 
- 	printf("%s\n", argv[1]);
+
+	char* flag = argv[1];
+	char* numpoints = argv[2];
+	char* numtrials = argv[3];
+	char* dimensions = argv[4];
+
+ 	printf("%s, %s, %s, %s\n", argv[1], argv[2], argv[3], argv[4]);
 
   return 0;
 }
