5ce86f1605c3dfc933281edd9456406618ad1114
jcuanm <cuanmartinez@college.harvard.edu>
2016-02-15 17:56:19 +0000

    Starting creating the linked lists

--- mst.c
+++ mst.c
@@ -23,17 +23,42 @@ int main(int argc, char *argv[])
 
 	// Creates a list of Node pointers of size numpoints
 	struct node* vertices[numpoints];
-/*
-	for(int i = 0; i < numpoints; i++) {
+	
+	int i;
+
+	for(i= 0; i < numpoints; i++) {
+		
 		struct node* vertex = malloc( sizeof(struct node) );
 		vertex->name = i;
 		vertex->weight = 2;
 		vertex->searched = false;
-
-		printf("name is: %f\n", vertex->weight);
+		
+		vertices[i] = vertex;
 	}
+	
+	// Creates a linked list at each index
+	int k;
+	int l;
+	
+	for(k = 0; k<numpoints; k++){
+		vertices[k] -> next = NULL;
+		struct node* head = malloc(sizeof(struct node));
+		struct node* head -> next = NULL;
+		
 
- 	printf("%s, %i, %s, %s\n", flag, numpoints, numtrials, dimensions);*/
+		/*
+		for (l = k + 1; l<numpoints;l++){
+			new_vertex = vertices[l]; 
+			
+		}*/
+	}
+	
+	
+	int j;
+	
+	for (j = 0; j < numpoints; j++){
+		printf("name is: %f\n", vertices[j]->weight);
+	}
 
 	return 0;
 }
