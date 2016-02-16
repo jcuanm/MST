88b340c43f20269bac763c655d3c7e43f09ef975
jcuanm <cuanmartinez@college.harvard.edu>
2016-02-16 13:15:30 +0000

    Updated Master

--- mst.c
+++ mst.c
@@ -10,7 +10,7 @@ struct node {
 	int name;
 	float weight;
 	bool searched;
-	struct Node* next;
+	struct node* next;
 };
  
 int main(int argc, char *argv[])
@@ -26,6 +26,45 @@ int main(int argc, char *argv[])
 	
 	int i;
 
+
+	for(i= 0; i < numpoints; i++) {
+		
+		struct node* vertex = malloc( sizeof(struct node) );
+		vertex->name = i;
+		vertex->weight = 2;
+		vertex->searched = false;
+		
+		//inserting the nodes into the array
+		vertices[i] = vertex;
+	}
+	
+	// Creates a linked list at each index
+	// The version of C on Cloud9 doesn't support variable declarations inside of the for loop
+	int k;
+	int l;
+	
+	for(k = 0; k<numpoints; k++){
+		struct node* tail = malloc(sizeof(struct node));
+		tail -> next = NULL;
+		vertices[k] -> next = tail;
+
+		for (l = k + 1; l<numpoints;l++){
+			struct node* new_vertex = malloc(sizeof(struct node));
+			new_vertex = vertices[l];
+			// The commented line below is giving me issues. I'm trying to get new_vertex to point to the same thing that the node in the array is pointin to.
+			//new_vertex -> next = vertices[k];
+			vertices[k] -> next = new_vertex;
+		}
+	}
+	
+	
+	int j;
+	
+	for (j = 0; j < numpoints; j++){
+		printf("name is: %f\n", vertices[j]->weight);
+	}
+
+
 	for(i= 0; i < numpoints; i++) {
 		
 		struct node* vertex = malloc( sizeof(struct node) );
@@ -35,12 +74,9 @@ int main(int argc, char *argv[])
 
 		printf("name is: %f\n", vertex->weight);
 	}
-	
-
-	
-	
 
  	printf("%s, %i, %s, %s\n", flag, numpoints, numtrials, dimensions);
+
 
 	return 0;
 }
