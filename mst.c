dad7c0f0ef5b636b10fe870209b3ee4e155aa3a9
William Long <wlong0827@gmail.com>
2016-02-17 17:50:47 -0500

    Mergesort

--- mst.c
+++ mst.c
@@ -13,17 +13,139 @@ struct node {
 	struct node* next;
 };
 
-int length(struct node* head) {
+int listLength(struct node* head) {
 	struct node* pointer = head;
 	int counter = 0;
 
 	while(pointer->next != NULL) {
 		counter++;
 		pointer = pointer->next;
+		printf("%i\n", pointer->name);
 	}
 
 	return counter;
 }
+
+struct node* SortedMerge(struct node* a, struct node* b);
+void FrontBackSplit(struct node* source,
+          struct node** frontRef, struct node** backRef);
+
+/* sorts the linked list by changing next pointers (not weight) */
+void MergeSort(struct node** headRef)
+{
+  struct node* head = *headRef;
+  struct node* a;
+  struct node* b;
+ 
+  /* Base case -- length 0 or 1 */
+  if ((head == NULL) || (head->next == NULL))
+  {
+    return;
+  }
+ 
+  /* Split head into 'a' and 'b' sublists */
+  FrontBackSplit(head, &a, &b); 
+ 
+  /* Recursively sort the sublists */
+  MergeSort(&a);
+  MergeSort(&b);
+ 
+  /* answer = merge the two sorted lists together */
+  *headRef = SortedMerge(a, b);
+}
+ 
+/* See http://geeksforgeeks.org/?p=3622 for details of this 
+   function */
+struct node* SortedMerge(struct node* a, struct node* b)
+{
+  struct node* result = NULL;
+ 
+  /* Base cases */
+  if (a == NULL)
+     return(b);
+  else if (b==NULL)
+     return(a);
+ 
+  /* Pick either a or b, and recur */
+  if (a->weight <= b->weight)
+  {
+     result = a;
+     result->next = SortedMerge(a->next, b);
+  }
+  else
+  {
+     result = b;
+     result->next = SortedMerge(a, b->next);
+  }
+  return(result);
+}
+ 
+/* UTILITY FUNCTIONS */
+/* Split the nodes of the given list into front and back halves,
+     and return the two lists using the reference parameters.
+     If the length is odd, the extra node should go in the front list.
+     Uses the fast/slow pointer strategy.  */
+void FrontBackSplit(struct node* source,
+          struct node** frontRef, struct node** backRef)
+{
+  struct node* fast;
+  struct node* slow;
+  if (source==NULL || source->next==NULL)
+  {
+    /* length < 2 cases */
+    *frontRef = source;
+    *backRef = NULL;
+  }
+  else
+  {
+    slow = source;
+    fast = source->next;
+ 
+    /* Advance 'fast' two nodes, and advance 'slow' one node */
+    while (fast != NULL)
+    {
+      fast = fast->next;
+      if (fast != NULL)
+      {
+        slow = slow->next;
+        fast = fast->next;
+      }
+    }
+ 
+    /* 'slow' is before the midpoint in the list, so split it in two
+      at that point. */
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = NULL;
+  }
+}
+ 
+/* Function to print nodes in a given linked list */
+void printList(struct node *node)
+{
+  while(node!=NULL)
+  {
+   printf("%f ", node->weight);
+   node = node->next;
+  }
+}
+ 
+/* Function to insert a node at the beginging of the linked list */
+void push(struct node** head_ref, int new_weight)
+{
+  /* allocate node */
+  struct node* new_node =
+            (struct node*) malloc(sizeof(struct node));
+  
+  /* put in the weight  */
+  new_node->weight  = new_weight;
+  
+  /* link the old list off the new node */
+  new_node->next = (*head_ref);    
+  
+  /* move the head to point to the new node */
+  (*head_ref)    = new_node;
+} 
  
 int main(int argc, char *argv[])
 {
@@ -84,7 +206,10 @@ int main(int argc, char *argv[])
 		printf("\n");
 	}
 
-	printf("%i\n", length(vertices[0]));
+	struct node** pointer = &vertices[0];
+
+	MergeSort(pointer);
+	listLength(vertices[0]);
 
 	return 0;
 }
